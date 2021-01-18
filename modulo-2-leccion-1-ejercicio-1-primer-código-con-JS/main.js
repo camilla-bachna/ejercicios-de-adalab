cc512db4025181e7a51f7ac4163c5c54b94c0575
camilla-bachna <camilla.bachna@gmail.com>
2021-01-18 17:41:26 +0100

    Frist code with JS

---
+++ modulo-2-leccion-1-ejercicio-1-primer-código-con-JS/main.js
@@ -0,0 +1,5 @@
+/* Aqui va el código JavaScript */
+
+'use strict';
+
+document.querySelector('h1').innerHTML = '¡Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente!';
