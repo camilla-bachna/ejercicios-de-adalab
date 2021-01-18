73be34b495001ce55d594f74227f7d4fc60b2bf5
camilla-bachna <camilla.bachna@gmail.com>
2021-01-18 19:04:05 +0100

    variables and constantes

---
+++ modulo-2-leccion-1-ejercicio-2-const-let/main.js
@@ -0,0 +1,10 @@
+/* Aqui va el código JavaScript */
+
+'use strict'; 
+
+const name = "Camilla";
+name = "Susi";    /* main.js:6 Uncaught TypeError: Assignment to constant variable */
+
+let homeAddress;
+homeAddress = "Calle Mayor, 7";
+homeAddress = "Calle de Toledo, 7";
