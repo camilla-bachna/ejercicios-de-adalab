76232f9308aea55dc09979380d20e15a60359908
camilla-bachna <camilla.bachna@gmail.com>
2021-01-18 19:21:30 +0100

    fix tipo

--- modulo-2-leccion-1-ejercicio-4-seleccion-adalabers/main.js
+++ modulo-2-leccion-1-ejercicio-4-seleccion-adalabers/main.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const titleElement = document.querySelector(".title");
-let listlement = document.querySelector(".list__item--2");
-listlement = document.querySelector(".list__item--1");
+let listElement = document.querySelector(".list__item--2");
+listElement = document.querySelector(".list__item--1");
 
-titleElement.innerHTML = titleElement.innerHTML + " " + listlement.innerHTML;
+titleElement.innerHTML = titleElement.innerHTML + " " + listElement.innerHTML;
