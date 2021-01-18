b075d82edf2713386a3877a29342c9d107aa35ff
camilla-bachna <camilla.bachna@gmail.com>
2021-01-18 19:19:57 +0100

    add a list item to the title

---
+++ modulo-2-leccion-1-ejercicio-4-seleccion-adalabers/main.js
@@ -0,0 +1,8 @@
+
+'use strict';
+
+const titleElement = document.querySelector(".title");
+let listlement = document.querySelector(".list__item--2");
+listlement = document.querySelector(".list__item--1");
+
+titleElement.innerHTML = titleElement.innerHTML + " " + listlement.innerHTML;
