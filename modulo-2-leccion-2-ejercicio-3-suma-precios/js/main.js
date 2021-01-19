0d7063f0fe67d9da27d59b288176d4a8fb6168c0
camilla-bachna <camilla.bachna@gmail.com>
2021-01-19 20:07:53 +0100

    calculate sum with JS

---
+++ modulo-2-leccion-2-ejercicio-3-suma-precios/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const firstName = 'Eva';
+const surname = 'Maier';
+
+console.log(firstName + " " + surname);
+
+const priceKiwi = 5;
+const pricePear = 2;
+const priceGrape = 4;
+
+let sumPriceKiwi = priceKiwi * 2;
+let sumPricePear = pricePear * 3;
+let sumPriceGrape = priceGrape / 2;
+
+console.log (sumPriceKiwi + sumPricePear + sumPriceGrape);
