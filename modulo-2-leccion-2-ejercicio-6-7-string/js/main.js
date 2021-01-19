8233b55edd96846fcb9ee4a3d6f991426f977da7
camilla-bachna <camilla.bachna@gmail.com>
2021-01-19 22:27:32 +0100

    change string to template string

---
+++ modulo-2-leccion-2-ejercicio-6-7-string/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let firstName = "Lola";
+let greeting1 = "Hola ";
+let greeting2 = ", encantada de conocerte.";
+
+console.log(greeting1 + firstName + greeting2);
+
+firstName = "Laura";
+
+console.log(`Hola ${firstName}, encantada de conocerte.`);
+//Interpolación de cadenas (Template strings)
