2b74f30e4fbab9acd33cf4a9227c0d69dfb1adff
camilla-bachna <camilla.bachna@gmail.com>
2021-01-21 17:26:13 +0100

    add first function

---
+++ modulo-2-leccion-4-ejercicio-1-function/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function sum(a, b){
+    return a * b;
+}
+
+const costKiwi = sum (2, 5);
+console.log("5 kilos of kiwis cost", costKiwi);
+
+const costOrange = sum (3, 3);
+console.log('3 kilos of oranges cost', costOrange);
+
+const costApple = sum (1, 4);
+console.log('4 kilos of apples cost', costApple);
