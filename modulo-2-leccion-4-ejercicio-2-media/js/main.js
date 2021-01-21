67886c5f0dbaf43cce32b9c9d98f1c444505f11a
camilla-bachna <camilla.bachna@gmail.com>
2021-01-21 17:58:02 +0100

    write function to calculate average number

---
+++ modulo-2-leccion-4-ejercicio-2-media/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function sum(a, b, c, d){
+    return (a + b + c + d) / 4;
+}
+
+let averageNumber = sum (1, 1, 1, 1);
+console.log(averageNumber);
+
+averageNumber = sum (2, 3, 4, 5);
+console.log(averageNumber);
+
+averageNumber = sum (12, 26, 64, 35);
+console.log(averageNumber);
