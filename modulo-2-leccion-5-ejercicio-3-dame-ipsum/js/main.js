aae502d9a1102363d9718c72e007271335b1dd15
camilla-bachna <camilla.bachna@gmail.com>
2021-01-24 22:02:21 +0100

    add += to add a new line and not replace

--- modulo-2-leccion-5-ejercicio-3-dame-ipsum/js/main.js
+++ modulo-2-leccion-5-ejercicio-3-dame-ipsum/js/main.js
@@ -7,8 +7,8 @@ const loremIpsum = document.querySelector('.js-paragraph');
 //handler
 
 function paragraph(){
-  loremIpsum.innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi omnis adipisci voluptate blanditiis atque assumenda nihil optio rem aspernatur, esse cum vero modi molestias quo vel perferendis molestiae architecto veniam!";
+  loremIpsum.innerHTML += "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi omnis adipisci voluptate blanditiis atque assumenda nihil optio rem aspernatur, esse cum vero modi molestias quo vel perferendis molestiae architecto veniam!";
 }
 
 // listener on the element, with event type and handler
-p.addEventListener('mouseover', paragraph);
+loremIpsum.addEventListener('mouseover', paragraph);
