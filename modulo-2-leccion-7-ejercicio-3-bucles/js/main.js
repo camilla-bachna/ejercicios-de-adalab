1cd3dbd3367baa10f71bf2f4cb8075b14f02dc06
camilla-bachna <camilla.bachna@gmail.com>
2021-01-26 18:59:40 +0100

    add another bucle

--- modulo-2-leccion-7-ejercicio-3-bucles/js/main.js
+++ modulo-2-leccion-7-ejercicio-3-bucles/js/main.js
@@ -1,11 +1,17 @@
 "use strict";
-
+/* 
 for (let acc = 0; acc < 20; acc = acc + 2) {
   
   console.log("El resultado es: " + acc);
-}
+} */
 
 /* 
 for (let i = 0; i < 20; i+=2) {
   console.log('Voy por la vuelta ' + i);
 } */
+
+let acc = 0;
+for (let i = 0; i < 10; i++) {
+  acc += 2;
+  console.log("El resultado es:  " + acc);
+}
