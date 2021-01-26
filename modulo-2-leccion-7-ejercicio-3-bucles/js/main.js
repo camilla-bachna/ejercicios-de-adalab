f516af4998c96bdb69a6d6e889ce2c4754f83313
camilla-bachna <camilla.bachna@gmail.com>
2021-01-26 18:05:54 +0100

    add bucle that executes 10 times and adds 2

---
+++ modulo-2-leccion-7-ejercicio-3-bucles/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+for (let acc = 0; acc < 20; acc = acc + 2) {
+  
+  console.log("El resultado es: " + acc);
+}
+
+/* 
+for (let i = 0; i < 20; i+=2) {
+  console.log('Voy por la vuelta ' + i);
+} */
