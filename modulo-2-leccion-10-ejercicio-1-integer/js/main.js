f226661319d31df73886cbb366c1bbaab8885395
camilla-bachna <camilla.bachna@gmail.com>
2021-02-01 20:18:24 +0100

    add parameters to URL

--- modulo-2-leccion-10-ejercicio-1-integer/js/main.js
+++ modulo-2-leccion-10-ejercicio-1-integer/js/main.js
@@ -1,14 +1,10 @@
 "use strict";
 
 function getEmoji() {
-  fetch("https://api.rand.fun/games/rockpaperscissorslizardspock")
+  fetch("https://api.rand.fun/number/integer?length=4")
     .then((response) => response.json())
     .then((data) => {
       document.querySelector(".js-result").innerHTML = data.result;
     });
 }
 document.querySelector(".js-emoji").addEventListener("click", getEmoji);
-
-/*   fetch('https://api.rand.fun/games/rockpaperscissorslizardspock')
-  .then(response => response.json())
-  .then(data => document.body.innerHTML = data.result); */
