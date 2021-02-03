d846ed2365e6b28a79b7831234870d12b99ae172
camilla-bachna <camilla.bachna@gmail.com>
2021-02-03 21:16:15 +0100

    add or remove picture depending on the clicked button

---
+++ modulo-2-leccion-12-ejercicio-2-viaje/js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+const containerElement = document.querySelector('.js-container');
+const buttonMadrid = document.querySelector('.js-Madrid');
+const buttonVienna = document.querySelector('.js-Vienna');
+const inputElement = document.querySelector('.js-input');
+
+//create images
+
+//Madrid
+
+const newImg = document.createElement('img');
+
+newImg.src = './images/madrid.jpg';
+
+newImg.style.width = '250px';
+newImg.style.margin = '10px';
+
+//Vienna
+
+const newImg2 = document.createElement('img');
+
+newImg2.src = './images/vienna.jpg';
+
+newImg2.style.width = '250px';
+newImg2.style.margin = '10px';
+
+function userClickMadrid() {
+  buttonMadrid.checked = true;
+  buttonVienna.checked = false;
+  containerElement.appendChild(newImg);
+  containerElement.removeChild(newImg2);
+}
+
+function userClickVienna() {
+  buttonMadrid.checked = false;
+  buttonVienna.checked = true;
+  containerElement.appendChild(newImg2);
+  containerElement.removeChild(newImg);
+}
+
+buttonMadrid.addEventListener('click', userClickMadrid);
+buttonVienna.addEventListener('click', userClickVienna);
