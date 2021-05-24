39cecebd0989b3c637bb93fe4513f589985bd7a0
camilla-bachna <camilla.bachna@gmail.com>
2021-05-24 10:45:08 +0200

    recipe depending on the ingredient - switch

---
+++ JavaScript/modulo-2-leccion-3-ejercicio-7-receta/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const ingredient = 'chicken';
+
+// aquí tu código
+
+switch (ingredient) {
+  case 'chicken':
+    console.log('Roast chicken with potatoes, carrots and mixed salat');
+    break;
+  case 'hake fish':
+    console.log('Fried hake fish with grilled vegetables');
+    break;
+  case 'spinach':
+    console.log('Spinach quiche');
+    break;
+  default:
+    console.log('There is nothing in the fridge');
+}
