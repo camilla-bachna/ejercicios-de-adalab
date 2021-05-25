ea33cf9ac0e83489935f17578da35650662bf148
camilla-bachna <camilla.bachna@gmail.com>
2021-05-25 17:39:19 +0200

    use remainder operator to check if odd or even

---
+++ JavaScript/modulo-2-leccion-4-ejercicio-4-par-impar/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* remainder operator (%) to obtain the remainder of the division between two values */
+
+function even(a) {
+  if (a % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(even(20));
