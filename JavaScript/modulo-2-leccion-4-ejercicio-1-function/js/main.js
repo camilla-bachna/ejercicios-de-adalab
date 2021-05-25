22e7b0ef59e078abde79456734b39f7f054c6339
camilla-bachna <camilla.bachna@gmail.com>
2021-05-25 16:49:47 +0200

    add comments

--- JavaScript/modulo-2-leccion-4-ejercicio-1-function/js/main.js
+++ JavaScript/modulo-2-leccion-4-ejercicio-1-function/js/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
-function sum(a, b){
-    return a * b;
+function sum(a, b) {
+  return a * b;
 }
 
-const costKiwi = sum (2, 5);
-console.log("5 kilos of kiwis cost", costKiwi);
+const costKiwi = sum(2, 5);
+// 2 price of kiwi
+console.log('5 kilos of kiwis cost', costKiwi);
 
-const costOrange = sum (3, 3);
+const costOrange = sum(3, 3);
+//3 price of oranges
 console.log('3 kilos of oranges cost', costOrange);
 
-const costApple = sum (1, 4);
+const costApple = sum(1, 4);
+//1 price of apples
 console.log('4 kilos of apples cost', costApple);
