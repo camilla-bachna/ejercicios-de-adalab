5a8ca08e4652744023a86817b923aea49b8cb01a
camilla-bachna <camilla.bachna@gmail.com>
2021-05-25 17:58:38 +0200

    use a function to avoid writing document.querySelector(...) many times

--- JavaScript/modulo-2-leccion-4-ejercicio-5-querySelector-para-todas/scripts/main.js
+++ JavaScript/modulo-2-leccion-4-ejercicio-5-querySelector-para-todas/scripts/main.js
@@ -1,12 +1,16 @@
-"use strict";
+'use strict';
 
-function getEl (a) {     
-    return document.querySelector(a);
+function getEl(a) {
+  return document.querySelector(a);
 }
 
+const headingEl = getEl('.heading');
+const greetingEl = getEl('.greeting');
+const textEl = getEl('.text');
+const btnEl = getEl('.btn');
 
-console.log(getEl(".btn"));
-
-const btnEl = getEl(".btn");
-
-btnEl.innerHTML = "Hola, púlsame.";
+headingEl.innerHTML = 'Hola';
+greetingEl.innerHTML = 'Que tal estas?';
+textEl.innerHTML =
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, impedit excepturi repellat fuga nostrum, doloribus ut delectus qui nulla voluptatibus rem velit quisquam. Expedita, aperiam consequatur. Officiis eveniet alias voluptate.';
+btnEl.innerHTML = 'Púlsame';
